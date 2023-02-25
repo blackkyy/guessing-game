46658aa61e5eead5057e80c0dc859f9c652716e1
Amal <yakubov.amal@gmail.com>
2023-02-25 17:11:08 +0500

    feat: finished task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+  array = [];
+  number;
+  setRange(min, max) {
+    for (let i = min + 1; i < max; i++) {
+      this.array.push(i);
     }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+    this.number = Math.ceil((this.array.length - 1) / 2);
+  }
+  guess() {
+    return this.array[this.number];
+  }
+  lower() {
+    this.array = this.array.slice(0, this.number);
+    this.number = Math.floor(this.array.length / 2);
+  }
+  greater() {
+    this.array = this.array.slice(this.number + 1);
+    this.number = Math.floor(this.array.length / 2);
+  }
 }
 
 module.exports = GuessingGame;
